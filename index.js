08768ff77ae8345f52444b13b1ad488ab351936d
Oliver Chiasson <olivierchiasson@hotmail.fr>
2021-12-07 17:23:02 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const { Plugin } = require('powercord/entities');
+const { getModule, React } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+const ChannelUserCount = require('./ChannelUserCount.jsx');
+
+class VoiceUserCount extends (
+  Plugin
+) {
+  async startPlugin() {
+    this.patchChannelItem();
+  }
+
+  pluginWillUnload() {
+    uninject('voice-user-count');
+  }
+
+  async patchChannelItem() {
+    const { countVoiceStatesForChannel } = await getModule([
+      'countVoiceStatesForChannel',
+    ]);
+    const ConnectedVoiceChannel = await getModule(m => m.default && m.default.displayName === 'ChannelItem');
+
+    const renderCount = (args, res) => {
+      let channel = args[0].channel;
+
+      if (!channel.isGuildVoice()) return res;
+      if (channel.userLimit) return res;
+      if (args[0].children[0]?.props.video) return res;
+
+      const userCount = countVoiceStatesForChannel(channel.id);
+
+      if (!userCount) return res;
+
+      const ChannelUserCountElement = React.createElement(ChannelUserCount, {
+        userCount,
+      });
+
+      args[0].children.push(ChannelUserCountElement);
+
+      return res;
+    };
+
+    inject('voice-user-count', ConnectedVoiceChannel, 'default', renderCount);
+
+    ConnectedVoiceChannel.default.displayName = 'ChannelItem';
+  }
+};
+
+module.exports = VoiceUserCount;
